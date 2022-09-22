e5d527880c70966d318d73792b6cfbce4fb3c19c
gulrose-seera <gul.rose1@seera.sa>
2022-09-22 15:03:20 +0400

    added identifier for home page

---
+++ Splito-main/SplitoUITests/Screens/HomeScreen.swift
@@ -0,0 +1,52 @@
+//
+//  HomeScreen.swift
+//  SplitoUITests
+//
+//  Created by Gul Rose on 22/09/2022.
+//
+
+import Foundation
+import XCTest
+
+struct SplitHomePO {
+    let application: XCUIApplication
+    let billAmountField: XCUIElement
+    let incrementButton: XCUIElement
+    let decrementButton: XCUIElement
+    let totalPersonsView: XCUIElement
+    let zeroPercentButton: XCUIElement
+    let tenPercentButton: XCUIElement
+    let fifteenPercentButton: XCUIElement
+    let twentyPercentButton: XCUIElement
+    let totalBillPerPersonView: XCUIElement
+    let calculateBillButton : XCUIElement
+    
+    init(app: XCUIApplication = XCUIApplication()) {
+        application = app
+        billAmountField = application.textFields[AccessibilityIdentifier.billAmountField.rawValue]
+        incrementButton = application.buttons[AccessibilityIdentifier.incrementButton.rawValue]
+        decrementButton = application.buttons[AccessibilityIdentifier.decrementButton.rawValue]
+        totalPersonsView = application.textFields[AccessibilityIdentifier.totalPersonsView.rawValue]
+        zeroPercentButton = application.buttons[AccessibilityIdentifier.zeroPercentButton.rawValue]
+        tenPercentButton = application.buttons[AccessibilityIdentifier.tenPercentButton.rawValue]
+        fifteenPercentButton = application.buttons[AccessibilityIdentifier.fifteenPercentButton.rawValue]
+        twentyPercentButton = application.buttons[AccessibilityIdentifier.twentyPercentButton.rawValue]
+        totalBillPerPersonView = application.textFields[AccessibilityIdentifier.totalBillPerPersonView.rawValue]
+        calculateBillButton = application.buttons[AccessibilityIdentifier.calculateBillButton.rawValue]
+    }
+}
+
+extension SplitHomePO {
+    enum AccessibilityIdentifier: String {
+        case billAmountField = "bill_entered_amount";
+        case incrementButton = "increment_person";
+        case decrementButton = "decrement_person";
+        case totalPersonsView = "total_person_in_bill";
+        case zeroPercentButton = "0_percent";
+        case tenPercentButton = "10_percent";
+        case fifteenPercentButton = "15_percent";
+        case twentyPercentButton = "20_percent";
+        case totalBillPerPersonView = "total_bill_each_person"
+        case calculateBillButton = "calculate_bill_button"
+    }
+}
